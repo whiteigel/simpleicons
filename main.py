03462f3dbd6437dbf8cb193940066a6d94a033d0
Sergei <sergei.volodin1@ibm.com>
2024-04-02 14:24:40 +0200

    Instructions edited

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ def get_button(brand):
         res = f"![{icon.title}](https://img.shields.io/badge/-{icon.slug}-{icon.hex}?logo={icon.slug}&logoColor=white)"
         return res
     else:
-        return "Бренд не найден в базе данных значков."
+        return "No such brand in Simpleicon DB."
 
 def main_menu():
-    brand_name = input("Введите имя бренда: ").lower()
+    brand_name = input("Enter brand name: ").lower()
     return get_button(brand_name)
 
 print(main_menu())
